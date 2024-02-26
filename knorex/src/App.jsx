394a84df306119d59ec933f149c2b5eea032f5c8
Shahid Raza <aizansami123@gmail.com>
2024-02-26 14:23:24 +0530

    updated

--- knorex/src/App.jsx
+++ knorex/src/App.jsx
@@ -1,67 +1,14 @@
-import React, { useState, useEffect } from "react";
-import { Route, Routes, useParams } from "react-router-dom";
-import DropdownMenu from "./Componnets/DropdownMenu";
-import WeatherForecast from "./Componnets/WeatherForecast"
-// import DropdownMenu from "./Components/DropdownMenu";
-// import WeatherForecast from "./Components/WeatherForecast";
-
-const cities = ["Ho Chi Minh", "Singapore", "Kuala Lumpur", "Tokyo", "Athens"];
+import React from "react";
+import WeatherForecast from "./Componnets/WeatherForecast";
 
 const App = () => {
-  const [selectedCity, setSelectedCity] = useState("");
-  const [forecast, setForecast] = useState([]);
-
-  useEffect(() => {
-    const fetchWeatherData = async () => {
-      if (selectedCity) {
-        try {
-          const apiKey = "eee3032eb92d5531511faf21cea0d3eb";
-
-          // Fetch forecast data for the selected city
-          const forecastResponse = await fetch(
-            `https://api.openweathermap.org/data/2.5/forecast?q=${selectedCity}&appid=${apiKey}&units=metric`
-          );
-
-          if (!forecastResponse.ok) {
-            throw new Error("Failed to fetch forecast data");
-          }
-
-          const forecastData = await forecastResponse.json();
-
-          // Extract and format forecast data
-          const formattedForecastData = forecastData.list
-            .slice(0, 8)
-            .map((item) => ({
-              date: item.dt_txt,
-              weather: item.weather[0].description,
-              temperature: Math.round(item.main.temp),
-              icon: item.weather[0].icon,
-            }));
-
-          setForecast(formattedForecastData);
-        } catch (error) {
-          console.error("Error fetching weather data:", error.message);
-          alert("Failed to fetch weather data. Please try again later.");
-        }
-      }
-    };
-
-    fetchWeatherData();
-  }, [selectedCity]);
-
   return (
-    <div>
-      <h1>Weather Forecast</h1>
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <DropdownMenu cities={cities} onSelectCity={setSelectedCity} />
-          }
-        />
-        <Route path="/forecast/:city" element={<WeatherForecast />} />
-      </Routes>
-    </div>
+    <>
+      <h1 style={{textAlign:'center'}}>Weather Forecast</h1>
+      <div>
+        <WeatherForecast />
+      </div>
+    </>
   );
 };
 
--- knorex/src/Componnets/WeatherForecast.jsx
+++ knorex/src/Componnets/WeatherForecast.jsx
@@ -1,43 +1,70 @@
 import React, { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
+import "../Componnets/WeatherForecast.css";
 
 const WeatherForecast = () => {
-  const { city } = useParams();
-  const [forecastData, setForecastData] = useState(null);
+  const [selectedCity, setSelectedCity] = useState("")
+  const [forecastData, setForecastData] = useState(null)
+  const [loading, setLoading] = useState(false) 
 
   useEffect(() => {
     const fetchWeatherData = async () => {
       try {
+        if (!selectedCity)
+         return 
+        setLoading(true)
+
         const apiKey = "eee3032eb92d5531511faf21cea0d3eb";
         const response = await fetch(
-          `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`
+          `https://api.openweathermap.org/data/2.5/weather?q=${selectedCity}&appid=${apiKey}&units=metric`
         );
 
         if (!response.ok) {
-          throw new Error("Failed to fetch weather data");
+          throw new Error("Failed to fetch weather data")
         }
 
         const data = await response.json();
         setForecastData(data);
       } catch (error) {
-        console.error("Error fetching weather data:", error);
+        console.error("Error fetching weather data:", error)
+        alert("Failed to fetch weather data. Please try again later.")
+      } finally {
+        setLoading(false); 
       }
     };
 
     fetchWeatherData();
-  }, [city]);
+  }, [selectedCity]);
 
-  if (!forecastData) {
-    return <div>Loading...</div>;
-  }
+  const handleSelectChange = (e) => {
+    setSelectedCity(e.target.value)
+  };
 
   return (
-    <div>
-      <h2>Weather Forecast for {city} for next 3 days</h2>
-      <p>Description: {forecastData.weather[0].description} &#9729;</p>
-      <p>Temperature: {forecastData.main.temp}°C &#9788;</p>
-      <p>Temperature: {forecastData.main.temp_max}°C &#9788;</p>
-      <p>Temperature: {forecastData.main.temp_min}°C &#9788;</p>
+    <div className="weather-forecast-container">
+      <div className="weather-forecast-select">
+        <label htmlFor="citySelect">Select a city:</label>
+        <select id="citySelect" onChange={handleSelectChange}>
+          <option value="">-- Select a city --</option>
+          <option value="Ho Chi Minh">Ho Chi Minh</option>
+          <option value="Singapore">Singapore</option>
+          <option value="Kuala Lumpur">Kuala Lumpur</option>
+          <option value="Tokyo">Tokyo</option>
+          <option value="Athens">Athens</option>
+        </select>
+      </div>
+      {loading ? ( 
+        <div>Loading...</div>
+      ) : (
+        forecastData && (
+          <div className="weather-forecast-details">
+            <h2>Weather Forecast for {selectedCity} for next 3 days</h2>
+            <p>Description: {forecastData.weather[0].description} &#9729;</p>
+            <p>Temperature: {forecastData.main.temp}°C &#9788;</p>
+            <p>Maximum Temperature: {forecastData.main.temp_max}°C &#9788;</p>
+            <p>Minimum Temperature: {forecastData.main.temp_min}°C &#9788;</p>
+          </div>
+        )
+      )}
     </div>
   );
 };
